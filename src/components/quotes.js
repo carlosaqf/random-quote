41ea38cdcb7afb031379528117dbd24871458ea0
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-05-17 10:19:38 -0500

    created quotes.js file

---
+++ src/components/quotes.js
@@ -0,0 +1,31 @@
+export const quotes = [
+    {
+        quote: "Together we can change the world, just one random act of kindness at a time.",
+        author: "Ron Hall"
+    },
+    {
+        quote: "Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you.",
+        author: "Princess Diana"
+    },
+    {
+        quote: "Goals transform a random walk into a chase.",
+        author: "Mihaly Csikszentmihalyi"
+    },
+    {
+        quote: "It is necessary to fall in love... if only to provide an alibi for all the random despair you are going to feel anyway.",
+        author: "Albert Camus"
+    },
+    {
+        quote: "We can all fight against loneliness by engaging in random acts of kindness.",
+        author: "Gail Honeyman"
+    },
+    {
+        quote: "I believe in fate. Sometimes that means an old bearded guy sitting on a cloud and pulling the strings; sometimes it means random atoms swirling through a cheerless universe; sometimes it means everything being preordained thanks to your karma credit from your previous lives.",
+        author: "Kyle MacLachlan"
+    },
+    {
+        quote: "I actually think that the most efficacious way of making a difference is to lead by example, and doing random acts of kindness is setting a very good example of how to behave in the world.",
+        author: "Misha Collins"
+    }
+];
+
