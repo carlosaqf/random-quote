464d85e2a63a59b957ecd16fbb654421efb2082d
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-05-17 10:13:48 -0500

    removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { device } from './components/devices'
+// import { device } from './components/devices'
 import styled from 'styled-components'
 import QuoteBox  from './components/QuoteBox'
 import { COLOR } from './components/color'
@@ -9,6 +9,7 @@ const AppHeader = styled.h1`
   color: ${COLOR.YELLOW};
   font-size: 3em;
   margin: 0.5em auto;
+  
 `;
 
 const AppContainer = styled.div`
